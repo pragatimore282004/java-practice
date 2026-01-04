a78226b1bf18fa2d8b015a0010eb96cf4dabc585
Pragati <pragatimore282004@gmail.com>
2026-01-04 11:28:20 +0530

    decimal to binary conversion

---
+++ DecimaltoBinary.java
@@ -0,0 +1,21 @@
+public class DecimaltoBinary {
+
+    public static void decimaltobinary(int n) {
+        int Mynum = n;
+        int pow = 0;
+        int binnum = 0;
+
+        while (n > 0) {
+            int rem = n % 2;
+            binnum = binnum + (rem * (int) Math.pow(10, pow));
+            pow++;
+            n = n / 2;   
+        }
+
+        System.out.println("Binary of " + Mynum + " = " + binnum);
+    }
+
+    public static void main(String[] args) {
+        decimaltobinary(5);
+    }
+}
