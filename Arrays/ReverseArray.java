1b00722157946219ee87c592ff0960d416765b6c
Pragati <pragatimore282004@gmail.com>
2026-01-05 22:18:43 +0530

    reverse the array

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,22 @@
+public class ReverseArray {
+    public static void reverseArray(int numbers[]){
+     int first=0;
+     int last=numbers.length-1;
+     while(first<last){
+        //swapping the numbers
+        int temp=numbers[last];
+        numbers[last]=numbers[first];
+        numbers[first]=temp;
+        first++;
+        last--;
+     }
+    }
+    public static void main(String args[]){
+    int numbers[]={2,4,6,8,10};
+    reverseArray(numbers);
+    for(int i=0;i<numbers.length;i++){
+        System.out.println(numbers[i]);
+    }
+    System.out.println();
+    }
+}
