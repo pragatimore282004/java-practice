794c70e863c0dc4424817ae5a54b9b87e9f07b9c
Pragati <pragatimore282004@gmail.com>
2026-01-07 23:38:39 +0530

    butterfly star pattern

---
+++ ButterflyPattern.java
@@ -0,0 +1,42 @@
+public class ButterflyPattern{
+    public static void butterfly(int n) {
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // 1st half//inner loop star=i
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // star
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // 2nd half
+        for (int i = n; i >= 1; i--) {
+            // inner loop star
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spaces
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // star
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String args[]){
+        butterfly(4);
+    }
+}
+
+    
