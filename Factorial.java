58537185605cd646164793abf4c15e5a2f9f843d
Pragati <pragatimore282004@gmail.com>
2026-01-20 23:22:24 +0530

    Factorial

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Factorial{
+    public static int factorial(int n){
+        int f=1;
+        for(int i=1;i<n;i++){
+            f=f*i;
+        }
+        return f;
+    }
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter n");
+    int n=sc.nextInt();
+    System.out.println("Factorial of "+ n + " is : " +factorial(n));
+    }
+}
