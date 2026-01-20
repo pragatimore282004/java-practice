1538049fcad1d913a55908608473fc7da0116b9b
Pragati <pragatimore282004@gmail.com>
2026-01-20 23:07:51 +0530

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Calculator {
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.println("enter a");
+      int a=sc.nextInt();
+      System.out.println("enter b");
+      int b=sc.nextInt();
+      System.out.println("enter operator");
+      char operator=sc.next().charAt(0);
+
+      switch(operator){
+        case '+':System.out.println("addition is:"+ (a+b));
+        break;
+        case '-':System.out.println("substraction is:"+ (a-b));
+        break;
+         case '*':System.out.println("multiplication is:"+(a*b));
+        break;
+         case '/':System.out.println("division is:"+(a/b));
+        break;
+         case '%':System.out.println("modulo is:"+(a%b));
+        break;
+      }
+    }
+}
