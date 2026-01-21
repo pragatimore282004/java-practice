c495de85ec7c8230e9434a4e10c130333fb936e5
Pragati <pragatimore282004@gmail.com>
2026-01-21 10:26:15 +0530

    Print Reverse of Number

---
+++ ReverseNum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ReverseNum{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n");
+        int n=sc.nextInt();
+        while(n>0){
+            int lastdigit=n%10;
+            System.out.print(lastdigit);
+            n=n/10;
+        }
+        System.out.println();
+    }
+}
