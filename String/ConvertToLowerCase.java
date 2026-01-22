fdd37e7ef13a3aa77b7bdf6cf51f45c26015cd33
Pragati <pragatimore282004@gmail.com>
2026-01-22 17:13:59 +0530

    Convert String To LowerCase

---
+++ String/ConvertToLowerCase.java
@@ -0,0 +1,7 @@
+public class ConvertToLowerCase {
+    public static void main(String args[]){
+        String str="PRAGATI MORE";
+        String lower=str.toLowerCase();
+        System.out.println(lower);
+    }
+}
