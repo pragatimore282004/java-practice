1ea9c3004e151c9d4054bcc2f294e3704ba79944
Pragati <pragatimore282004@gmail.com>
2026-01-27 20:33:04 +0530

    Get Ith Bit

---
+++ Bit Manipulation/GetItBit.java
@@ -0,0 +1,15 @@
+public class GetItBit {
+    public static int getitbit(int n,int i){
+        int bitmask=1<<i;
+        if((n & bitmask)==0){
+            return 0;
+        }
+        else{
+            return 1;
+        }
+}
+    public static void main(String args[]){
+    System.out.println("Bit at ith position : "+ getitbit(10,2));
+    }
+}
+
