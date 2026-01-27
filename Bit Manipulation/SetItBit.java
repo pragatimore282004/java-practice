b335e8a7878c5581efa87627e71e5fa37c3bc630
Pragati More <pragatimore282004@gmail.com>
2026-01-27 23:57:54 +0530

    SetItBit.java

--- Bit Manipulation/SetItBit.java
+++ Bit Manipulation/SetItBit.java
@@ -1,7 +1,7 @@
 public class SetItBit {
     public static int setitbit(int n,int i){
         int bitmask=1<<i;
-        return bitmask|n;
+        return n|bitmask;
     }
     public static void main(String args[]){
         System.out.println(setitbit(10,2));
